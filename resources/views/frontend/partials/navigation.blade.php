f6082ebb600823a9338eac10a6a72df9ec1b0411
ddan9690 <dancanokeyo08@gmail.com>
2024-03-11 16:09:57 +0300

    minor updates

--- resources/views/frontend/partials/navigation.blade.php
+++ resources/views/frontend/partials/navigation.blade.php
@@ -12,7 +12,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-0">
-                <a href="#" class="nav-item nav-link active">Home</a>
+                <a href="/" class="nav-item nav-link active">Home</a>
                 {{-- <a href="#" class="nav-item nav-link">About</a> --}}
                 <a href="/features" class="nav-item nav-link">Features</a>
                 {{-- <a href="#" class="nav-item nav-link">Analysis</a>
